444cd68cc6260cc4ba88cae772082de3c2827f15
Victorinox2 <uwaomaviktor@yahoo.com>
2023-03-12 14:42:44 -0700

    if else

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-#11-pow.py
+# 11-pow.py
 
-"""Write a function that computes a to the power of b and return the value."""
-def pow(a,b):
-    return(a**b)
+
+def pow(a, b):
+    """Return a to the power of b."""
+    return (a ** b)
