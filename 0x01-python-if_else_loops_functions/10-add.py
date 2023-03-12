65391e61710d2af950cce512bfe14d32581a50f2
Victorinox2 <uwaomaviktor@yahoo.com>
2023-03-12 14:08:49 -0700

    else if

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,10 +1,7 @@
-#!/usr/bin/env python3
-#10-add.py
+#!/usr/bin/python3
+# 10-add.py
 
-"""Write a function that adds two integers and returns the result."""
 
-def add(a,b):
-	a;
-	b;
-	sum == a+b;
-	return(sum)
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
