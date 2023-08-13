a1f4c6296efe3f224e99226eaf47fcc3628f44ad
Victorinox2 <uwaomaviktor@yahoo.com>
2023-08-13 07:15:32 -0700

    upadate

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,6 @@
-/**
- * File: 0-positive_or_negative.py
- * Auth: Victorinox2
- */
 #!/usr/bin/python3
-
+import random
+number = random.randint(-10, 10)
 if (number > 0):
     print("{} is positive".format(number))
 elif (number == 0):
