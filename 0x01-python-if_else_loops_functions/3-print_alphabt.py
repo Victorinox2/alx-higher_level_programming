7b4f0c787709b2f503cdaefd3a115224c597abe7
Victorinox2 <uwaomaviktor@yahoo.com>
2023-08-13 17:35:19 -0700

    update

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# File: 3-print_alphabt.py
+# Auth: Victorinox2
+
+for num in range(97, 123):
+    print("{}".format(chr(num)), end="")
+if chr(num == 113) && chr(num == 101):
+    continue
