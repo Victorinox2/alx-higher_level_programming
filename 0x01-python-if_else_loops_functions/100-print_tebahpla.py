44fb1a9651ec97ccf64742da4701d6c29a04b5c4
Victorinox2 <uwaomaviktor@yahoo.com>
2023-08-14 22:10:47 -0700

    update

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# File: 100-print_tebahpla.py
+# Auth: Victorinox2
+
+i = 0
+for x in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c-i)), end="")
+    i = 32 if i == 0 else 0
