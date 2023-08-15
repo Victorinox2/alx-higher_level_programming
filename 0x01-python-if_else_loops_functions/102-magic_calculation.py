cc7227b179963259aa1aa2b07f9c48c2310e3c0a
Victorinox2 <uwaomaviktor@yahoo.com>
2023-08-14 22:53:24 -0700

    update

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# File: 102-magic_calculation.py
+# Auth: Victorinox2
+
+def magic_calculation(a,b,c):
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b-c)
