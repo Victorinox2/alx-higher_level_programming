eec7ef6ca185deb0f0f043064cd12f7f50982152
Victorinox2 <uwaomaviktor@yahoo.com>
2023-08-15 23:53:24 -0700

    module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,7 +5,7 @@
 
 if __name__ == "__main__":
     """prints the addition of 1 and 2"""
-    from add import add_0
+    from add_0 import add
     a = 1
     b = 2
-    print("".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
