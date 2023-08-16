fedd5f384564c5909c275514c83cd9089398bf1d
Victorinox2 <uwaomaviktor@yahoo.com>
2023-08-15 22:57:41 -0700

    module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,8 +4,13 @@
 # Auth: Victorinox2
 
 if __name__ == "__main__":
+
     """Prints the addition of 1 and 2"""
+
+    import add_0
+
+    addition = add_0.add(a, b)
 
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, addition)))
