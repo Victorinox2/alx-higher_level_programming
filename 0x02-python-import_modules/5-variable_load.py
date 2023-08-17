11420264ea7123617eee6cdd43986f720f637d0a
Victorinox2 <uwaomaviktor@yahoo.com>
2023-08-16 20:42:48 -0700

    module

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# File: 5-variable_load.py
+# Auth: Victorinox2
+
+if __name__ == "__main__":
+    """prints the value of variable a from variable_load_5"""
+    from variable_load_5 import a
+
+    print(a)
