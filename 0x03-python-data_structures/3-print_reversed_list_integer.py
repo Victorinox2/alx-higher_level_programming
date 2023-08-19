b99f67c488f6d021ee19856410e5e218934fdeb8
Victorinox2 <uwaomaviktor@yahoo.com>
2023-08-19 12:56:21 -0700

    data struct

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# File: 3-print_reversed_list_integer.py
+# Auth: Victorinox2
+
+def print_reversed_list_integer(my_list=[]):
+    """print all integers of list in reverse."""
+    if isinstance(my_list,list):
+        my_list.reverse()
+        for x in my_list:
+            print("{:d}".format(x))
