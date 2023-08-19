3a37a4ca4aeeea48a1f38898a7130438696b8634
Victorinox2 <uwaomaviktor@yahoo.com>
2023-08-19 12:23:07 -0700

    data struct

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# File: 1-element_at.py
+# Auth: Victorinox2
+
+def element_at(my_list, idx):
+    """Retrieve an element from list"""
+    if idx < 0 or idx > (len(my_list) -1):
+        return None
+    return(my_list[idx])
