4cc723146292aa2c42277994c1cd911d9fdc831e
Victorinox2 <uwaomaviktor@yahoo.com>
2023-08-19 13:08:36 -0700

    data struct

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# File: 4-new_in_list.py
+# Auth: Victorinox2
+
+def new_in_list(my_list, idx, element):
+    """Replace an element in a list at a specific position"""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return (my_list)
+
+    copy = [x for x in my_list]
+    copy[idx] = element
+    return(copy)
