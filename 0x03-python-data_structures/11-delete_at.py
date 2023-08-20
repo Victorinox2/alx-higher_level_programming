1a484969223b3a9ff082e3215a4244bccfc0d6ff
Victorinox2 <uwaomaviktor@yahoo.com>
2023-08-19 21:49:14 -0700

    data struct

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -6,4 +6,4 @@ def delete_at(my_list=[], idx=0):
     """delete an item at a specific position"""
     if idx >= 0 and idx < len(my_list):
         del my_list[idx]
-        return (my_list)
+    return (my_list)
