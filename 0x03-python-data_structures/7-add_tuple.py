7cd8a1e30d62c514d3c55fb8f99160d43f0f7a1d
Victorinox2 <uwaomaviktor@yahoo.com>
2023-08-19 20:58:47 -0700

    data structure

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# File:7-add_tuple.py 
+# 7-add_tuple.py
 # Auth: Victorinox2
 
 def add_tuple(tuple_a=(), tuple_b=()):
@@ -15,4 +15,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
         else:
             tuple_b = tuple_b[0], 0
 
-return(tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return(tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
