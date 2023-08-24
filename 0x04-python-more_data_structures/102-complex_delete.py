b911829a50fe674d8ba1621da753c37f35a9616a
Victorinox2 <uwaomaviktor@yahoo.com>
2023-08-24 07:31:11 -0700

    more data struct

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -7,7 +7,7 @@ def complex_delete(a_dictionary, value):
     while value in a_dictionary.values():
         for x, y in a_dictionary.items():
             if y == value:
-                del a_distance[k]
+                del a_dictionary[k]
                 break
 
     return (a_dictionary)
