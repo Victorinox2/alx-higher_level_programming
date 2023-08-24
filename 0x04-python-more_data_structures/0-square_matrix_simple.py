86388a97d2c8eb4d8569ef995a5054aba788f6e4
Victorinox2 <uwaomaviktor@yahoo.com>
2023-08-24 04:42:51 -0700

    more data struct

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,4 +4,4 @@
 
 def square_matrix_simple(matrix=[]):
     """Computes the square value of all integers of a matrix"""
-    return ([list(map(lamda x: x * x, row)) for row in matrix])
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
