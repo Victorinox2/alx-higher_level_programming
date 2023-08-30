44fc18a0c44a40c2c268e1de3d11ed9dcbcd49f6
Victorinox2 <uwaomaviktor@yahoo.com>
2023-08-30 23:16:30 +0000

    except

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,10 +2,19 @@
 # 1-safe_print_integer.py
 # Victorinox2
 
+
 def safe_print_integer(value):
+    """Print an integer with "{:d}".format().
+
+    Args:
+        value (int): The integer to print.
+
+    Returns:
+        If a TypeError or ValueError occurs - False.
+        Otherwise - True.
+    """
     try:
         print("{:d}".format(value))
-    except(ValueError, TypeError):
-        return False
-    else:
-        return True
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
