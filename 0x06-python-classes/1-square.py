82708a55b34125fcb988f7e7888ecb1c99178e3f
Victorinox2 <uwaomaviktor@yahoo.com>
2023-08-31 00:12:39 +0000

    class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 1-square.py
+# Victorinox2
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of new square.
+        """
+        self.__size = size
