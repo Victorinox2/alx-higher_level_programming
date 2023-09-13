fa246f67eee38cdaef413df0eb602e5950e0152d
Victorinox2 <uwaomaviktor@yahoo.com>
2023-09-13 05:25:41 -0700

    inheritance

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# Victorinox2
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,3 @@
+1.Superclass, baseclass, parentclass
+2.Subclass
+3.Inheritance
