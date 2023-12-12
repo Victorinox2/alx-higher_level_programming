5c2611f141a0db3328a30138ccd890fa27df8e6f
Victorinox2 <uwaomaviktor@yahoo.com>
2023-12-12 04:00:10 -0800

    python network

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# This script takes a URL as an argument, sends a GET request, and displays the body of 200 status code response.
+
+curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q "200" && curl -s "$1"
