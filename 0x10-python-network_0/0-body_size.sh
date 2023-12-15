baa2b0d3f81fd3690427f1b3e23d12aa1300c2e9
Victorinox2 <uwaomaviktor@yahoo.com>
2023-12-15 02:32:44 -0800

    Python Network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script takes a URL as an argument, sends a silent request, and displays the size of the response body in bytes.
-
-curl -sI "$1" | grep -i content-length | awk '{print $2}' |
+# takes in a URL, send a request to that URL, and displays the size of the body of the response
+curl -s "$1" | wc -c
