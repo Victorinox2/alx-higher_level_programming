2e55103a4403b8d3e466f6f4ada1056bc0e80dbd
Victorinox2 <uwaomaviktor@yahoo.com>
2023-12-15 03:01:34 -0800

    Python Network

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# This script takes a URL as argument, sends a GET request with a specific header, and displays the body of the response.
-
-url=$1
-user_id_header="X-School-User-Id: 98"
-
-curl -s -H "$user_id_header" "$url"
+# takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+curl -sH "X-School-User-Id: 98" "$1"
